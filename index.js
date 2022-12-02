4e35f8fc88d9f0bbcfa8aca8a84ebf01e9f2a360
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-12-02 13:50:16 -0800

    added questions to index.js

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
-const managerQuestions = [{
-    type: 'input',
-    name: 'name',
-    message: "What is the manager's name?"
-}]
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the manager's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is their email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the office number?",
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the engineer's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is their email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is their GitHub username?",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Intern's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is their email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school do they go to?",
+  },
+];
 
 class AskQuestions {
-    constructor(questions){
-        this.questions = questions;
-        this.data;
-        this.prompt();
-    }
+  constructor(questions) {
+    this.questions = questions;
+    this.data;
+    this.prompt();
+  }
 
-    prompt() {
-        inquirer.prompt(this.questions).then(answers => {
-            this.data = answers
-        })
-    }
+  prompt() {
+    inquirer.prompt(this.questions).then((answers) => {
+      this.data = answers;
+      this.info();
+    });
+  }
+
+  info() {
+    return this.data;
+  }
 }
+
+const ask = () => {
+   const managerPrompt = new AskQuestions(managerQuestions);
+   managerPrompt.data.name 
+};
+
+ask()
